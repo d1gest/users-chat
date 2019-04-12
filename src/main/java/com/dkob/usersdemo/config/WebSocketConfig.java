9192056d56d91fd40b3df91bf5fa4bc206e78686
dkobylki <dmitrii.kobylkin@t-systems.com>
2019-04-12 18:35:41 +0300

    Send to user

--- src/main/java/com/dkob/usersdemo/config/WebSocketConfig.java
+++ src/main/java/com/dkob/usersdemo/config/WebSocketConfig.java
@@ -1,12 +1,33 @@
 package com.dkob.usersdemo.config;
 
+import com.dkob.usersdemo.security.CustomUserDetailsService;
+import com.dkob.usersdemo.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -15,7 +36,37 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/chat");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String jwt = getJwtFromHeader(accessor);
+
+                    if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                        Long userId = tokenProvider.getUserIdFromJWT(jwt);
+
+                        UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        accessor.setUser(authentication);
+                    }
+                    return message;
+                }
+                return message;
+            }
+        });
+    }
+
+    private String getJwtFromHeader(StompHeaderAccessor accessor) {
+        String bearerToken = accessor.getNativeHeader("Authorization").get(0);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
--- src/main/java/com/dkob/usersdemo/controller/ChatController.java
+++ src/main/java/com/dkob/usersdemo/controller/ChatController.java
@@ -6,20 +6,30 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
+    @SendTo("/chat")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        chatMessage.setContent("FROM SERVER");
+        chatMessage.setContent(chatMessage.getContent());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.sendPrivateMessage")
+    @SendToUser("/chat")
+    public ChatMessage sendPrivateMessage(@Payload ChatMessage chatMessage, Principal pc) {
+        chatMessage.setContent(chatMessage.getContent());
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
+    @SendTo("/chat")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
